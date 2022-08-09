6745d648057f7641d0d4e817d23f7bcbce595526
Mohit Singh <mohit@mohitsingh.in>
2022-08-09 13:47:48 +0530

    feat: improve tree builder and matcher

--- src/path_extra.rs
+++ src/path_extra.rs
@@ -2,63 +2,41 @@ use std::path::{Component, Path, PathBuf};
 
 // Some helper methods to convert Path and PathBuf to String or str
 pub trait PathExtra {
-    fn x_base(&self) -> Option<String>;
-    fn x_parent(&self) -> Option<String>;
-    fn x_string(&self) -> String;
-    fn x_str(&self) -> &str;
+    fn base(&self) -> Option<String>;
+    fn to_string(&self) -> String;
 }
 
 impl PathExtra for Path {
-    fn x_base(&self) -> Option<String> {
+    fn base(&self) -> Option<String> {
         self.file_name()
             .and_then(|name| name.to_str())
             .map(|name| name.to_string())
     }
 
-    fn x_parent(&self) -> Option<String> {
-        self.parent()
-            .and_then(|p| p.to_str())
-            .map(|p| p.to_string())
-    }
-
-    fn x_str(&self) -> &str {
-        self.to_str().unwrap_or_default()
-    }
-
-    fn x_string(&self) -> String {
+    fn to_string(&self) -> String {
         self.to_str().map(|p| p.to_string()).unwrap_or_default()
     }
 }
 
 impl PathExtra for PathBuf {
-    fn x_base(&self) -> Option<String> {
+    fn base(&self) -> Option<String> {
         self.file_name()
             .and_then(|name| name.to_str())
             .map(|name| name.to_string())
     }
 
-    fn x_parent(&self) -> Option<String> {
-        self.parent()
-            .and_then(|p| p.to_str())
-            .map(|p| p.to_string())
-    }
-
-    fn x_str(&self) -> &str {
-        self.to_str().unwrap_or_default()
-    }
-
-    fn x_string(&self) -> String {
+    fn to_string(&self) -> String {
         self.to_str().map(|p| p.to_string()).unwrap_or_default()
     }
 }
 
 // Some helper methods to convert Path and PathBuf
 pub trait CompExtra {
-    fn x_str(&self) -> &str;
+    fn to_str(&self) -> &str;
 }
 
 impl CompExtra for Component<'_> {
-    fn x_str(&self) -> &str {
+    fn to_str(&self) -> &str {
         self.as_os_str().to_str().unwrap_or_default()
     }
 }
--- src/transformer.rs
+++ src/transformer.rs
@@ -1,24 +1,24 @@
 use crate::path_extra::*;
 use anyhow::{Error, Result};
-use serde_json::{json, to_string_pretty, Map};
+use serde_json::{json, to_string, Map, Value};
 use std::{collections::BTreeMap, path::PathBuf};
 
 pub fn as_path_names(dirs: Vec<PathBuf>) -> Vec<String> {
     dirs.into_iter()
-        .map(|d| d.x_base().unwrap_or_default())
+        .map(|d| d.base().unwrap_or_default())
         .collect()
 }
 
 pub fn as_paths(dirs: Vec<PathBuf>) -> Vec<String> {
-    dirs.into_iter().map(|d| d.x_string()).collect()
+    dirs.into_iter().map(|d| d.to_string()).collect()
 }
 
 pub fn as_json(dirs: Vec<PathBuf>) -> Result<String> {
     let map: BTreeMap<String, String> = dirs
         .into_iter()
-        .map(|d| (d.x_base().unwrap_or_default(), d.x_string()))
+        .map(|d| (d.base().unwrap_or_default(), d.to_string()))
         .collect();
-    serde_json::ser::to_string_pretty(&map).map_err(Error::msg)
+    to_string(&map).map_err(Error::msg)
 }
 
 fn common_path(dirs: &Vec<PathBuf>) -> PathBuf {
@@ -33,6 +33,18 @@ fn common_path(dirs: &Vec<PathBuf>) -> PathBuf {
         .collect::<PathBuf>()
 }
 
+fn add_children(target: &mut Value, val: Value) {
+    if let Some(target_obj) = target.as_object_mut() {
+        if let Some(_children) = target_obj.get_mut("_children") {
+            _children.as_array_mut().and_then(|arr| Some(arr.push(val)));
+        } else {
+            target_obj.insert("_children".to_string(), json!([val].to_vec()));
+        }
+    } else if let Some(arr) = target.as_array_mut() {
+        arr.push(val);
+    }
+}
+
 pub fn as_tree(dirs: Vec<PathBuf>) -> Result<String> {
     let common = common_path(&dirs);
     let val = dirs
@@ -41,41 +53,36 @@ pub fn as_tree(dirs: Vec<PathBuf>) -> Result<String> {
         .fold(json!(Map::new()), |mut acc, val| {
             let len = val.components().count();
             let mut comp_iter = val.components().into_iter().enumerate();
-            let original = common.join(val.clone()).x_string();
+            let original = common.join(val.clone()).to_string();
             let mut _ref = &mut acc;
             while let Some((count, p)) = comp_iter.next() {
-                let key = p.x_str();
                 let remaining = len - (count + 1);
-                if remaining >= 2 {
+                let current = p.to_str();
+                if remaining == 0 {
+                    let value = json!({
+                        "location": original,
+                        "label": current,
+                    });
+                    add_children(_ref, value)
+                } else if remaining > 1 {
                     _ref = _ref
                         .as_object_mut()
                         .unwrap()
-                        .entry(key)
+                        .entry(current)
                         .or_insert(json!(Map::new()));
                 } else if !_ref.is_null() {
                     let (_, base) = comp_iter.next().unwrap();
                     let value = json!({
                         "location": original,
-                        "label": base.x_str(),
+                        "label": base.to_str(),
                     });
                     if let Some(_obj) = _ref.as_object_mut() {
-                        if let Some(_key) = _obj.get_mut(key) {
-                            if let Some(_key_obj) = _key.as_object_mut() {
-                                if let Some(_children) = _key_obj.get_mut("_children") {
-                                    _children
-                                        .as_array_mut()
-                                        .and_then(|arr| Some(arr.push(value)));
-                                } else {
-                                    _key_obj
-                                        .insert("_children".to_string(), json!([value].to_vec()));
-                                }
-                            } else if let Some(arr) = _key.as_array_mut() {
-                                arr.push(value);
-                            }
+                        if let Some(_key) = _obj.get_mut(current) {
+                            add_children(_key, value);
                         } else {
                             _ref.as_object_mut()
                                 .unwrap()
-                                .insert(key.to_string(), json!([value].to_vec()));
+                                .insert(current.to_string(), json!([value].to_vec()));
                         }
                     } else if let Some(arr) = _ref.as_array_mut() {
                         arr.push(value);
@@ -84,5 +91,5 @@ pub fn as_tree(dirs: Vec<PathBuf>) -> Result<String> {
             }
             acc
         });
-    Ok(to_string_pretty(&val)?)
+    Ok(to_string(&val)?)
 }
